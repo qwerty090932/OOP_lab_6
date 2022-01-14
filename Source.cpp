4ee591230b9ba5e37c2197b592a4c7214deed040
qwerty090932 <92616711+qwerty090932@users.noreply.github.com>
2022-01-14 21:59:26 +1000

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,3 +1,5 @@
+// Предметная область - Больница
+// Сампилов Мунко. подгруппа 4
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,16 +8,16 @@ class human
 public:
 	string name;
 	string lastname;
-	human (string name, string lastname) // пвапваппав
+	human (string name, string lastname) // конструктор
 	{
 		this-> name = name;
 		this->lastname = lastname;
 	}
-	human(const human& other){ // êîíñòðóòîð êîïèðîâàíèÿ
+	human(const human& other){ // конструктор копирования
 		this->name = other.name;
 		this->lastname = other.lastname;
 	}
-	human& operator = (const human& rhs) { // îïåðàòîð ïðèñâàèâàíèÿ
+	human& operator = (const human& rhs) { // оператор присваивания
 		this->name = rhs.name;
 		this->lastname = rhs.lastname;
 
@@ -25,25 +27,25 @@ public:
 class doctor : public human
 {
 private:
-	string medical_specialties; // ñïåöèàëüíîñòü
-	string hospital; // ìåñòî ðàáîòû
+	string medical_specialties; // специальность
+	string hospital; // место работы
 public:
-	string workmanship; // êâàëèôèêàöèÿ 
+	string workmanship; // квалификация
 	
-	int countOfCurePatient; // êîë-âî âûëå÷åííûõ ïàöèåíòîâ
-	doctor(string name, string lastname, string medical_specialties,string hospital) :human(name, lastname) { // êîíñòðóêòîð
+	int countOfCurePatient; // кол-во вылеченных студентов
+	doctor(string name, string lastname, string medical_specialties,string hospital) :human(name, lastname) { // конструктор
 		this->name = name;
 		this->lastname = lastname;
 		this->medical_specialties = medical_specialties;
 		this->workmanship = workmanship;
 	}
-	doctor(const doctor& other):human(name,lastname) { // êîíñòðóòîð êîïèðîâàíèÿ
+	doctor(const doctor& other):human(name,lastname) { // конструктор копирования
 		this->name = other.name;
 		this->lastname = other.lastname;
 		this->medical_specialties = other.medical_specialties;
 		this->workmanship = workmanship;
 	}
-	doctor& operator = (const doctor& rhs) { // îïåðàòîð ïðèñâàèâàíèÿ
+	doctor& operator = (const doctor& rhs) { // оператор присваивания
 		this->name = rhs.name;
 		this->lastname = rhs.lastname;
 		this->medical_specialties = rhs.medical_specialties;
@@ -60,27 +62,27 @@ public:
 		countOfCurePatient = count;
 	}
 	template <typename T>
-	void setWorkmanship(T _workmanship) { // øàáëîí äëÿ ïîâûøåíèÿ êâàëèôèêàöèè èëè íàîáîðîò ïîíèæåíèÿ
+	void setWorkmanship(T _workmanship) { // шаблон для повышения квалификации или наоборот понижения
 		workmanship = _workmanship;
 	}
 };
 class patient : public human
 {
 private:
-	string hospital; // ïîëèêëèíèêà ïî ìåñòó æèòåëüñòâà
+	string hospital; // поликлиника по месту жительства
 public:
-	string choiceDoctor; // âûáîð ñïåöèàëèñòà, ê êàêîìó èìåííî âðà÷ó õî÷åò ïîïàñòü ïàöèåíò
-	patient(string name, string lastname, string choiceDoctor, string hospital) :human(name, lastname) { // êîíñòðóêòîð
+	string choiceDoctor; // выбор специалиста, к какому именно врачу хочет попасть пациент
+	patient(string name, string lastname, string choiceDoctor, string hospital) :human(name, lastname) { // конструкторв
 		this->choiceDoctor = choiceDoctor;
 		this->name = name;
 		this->lastname = lastname;
 	}
-	patient(const patient& other) :human(name, lastname) { // êîíñòðóòîð êîïèðîâàíèÿ
+	patient(const patient& other) :human(name, lastname) { // конструктор копирования
 		this->choiceDoctor = other.choiceDoctor;
 		this->name = other.name;
 		this->lastname = other.lastname;
 	}
-	patient& operator = (const patient& rhs) { // îïåðàòîð ïðèñâàèâàíèÿ
+	patient& operator = (const patient& rhs) { // оператор присваивания
 		this->name = rhs.name;
 		this->lastname = rhs.lastname;
 		this->choiceDoctor = rhs.choiceDoctor;
@@ -101,31 +103,31 @@ public:
 
 int main() {
 	setlocale(LC_ALL, "rus");
-	doctor doctor_1("Äàíèë","Äàíèëîâ","Îêóëèñò","2-àÿ ïîëèêëèíèêà");
-	doctor doctor_2("Íèêîëàé", "Íèêîëàåâ", "Òåðàïåâò", "1-àÿ ïîëèêëèíèêà");
+	doctor doctor_1("Данил","Данилов","Окулист","2-ая поликлиника");
+	doctor doctor_2("Николай", "Николаев", "Терапевт", "1-ая поликлиника");
 
 
 
-	patient patient_1("Èâàí", "Èâàíîâ", "Îêóëèñò","2-àÿ ïîëèêëèíèêà");
-	patient patient_2("Êèðèëë", "Èâàíîâ", "Îêóëèñò", "2-àÿ ïîëèêëèíèêà");
-	patient patient_3("Åâãåíèé", "Âîëêîâ", "Òåðàïåâò", "1-àÿ ïîëèêëèíèêà");
-	patient patient_4("Ìàêñèì", "Êðàâöîâ", "Îêóëèñò", "2-àÿ ïîëèêëèíèêà");
-	patient patient_5("Ìèõàèë", "Ìèõàéëîâ", "Òåðàïåâò", "2-àÿ ïîëèêëèíèêà");
-	patient patient_6("Àðò¸ì", "Áîðèñîâ", "Îêóëèñò", "1-àÿ ïîëèêëèíèêà");
+	patient patient_1("Иван", "Иванов", "Окулист","2-ая поликлиника");
+	patient patient_2("Кирилл", "Иванов", "Окулист", "2-ая поликлиника");
+	patient patient_3("Евгений", "Волков", "Терапевт", "1-ая поликлиника");
+	patient patient_4("Максим", "Кравцов", "Окулист", "2-ая поликлиника");
+	patient patient_5("Михаил", "Михайлов", "Терапевт", "2-ая поликлиника");
+	patient patient_6("Артём", "Борисов", "Окулист", "1-ая поликлиника");
 
 	if ((doctor_1.getHospital() == patient_1.getHospital())&&(patient_1.getchoiceDoctor() == doctor_1.getMedical_specialties())) {
-		cout << patient_1.name << " " << patient_1.lastname << " - " << "Çäîðîâ"<<endl;
+		cout << patient_1.name << " " << patient_1.lastname << " - " << "Здоров"<<endl;
 		doctor_1.countOfCurePatient++;
 	}
 	else {
-		cout << "Íå òîò âðà÷ èëè íå òà ïîëèêëèíèêà";
+		cout << "Не тот врач или не та поликлиника";
 	}
 	if ((doctor_2.getHospital() == patient_2.getHospital()) && (patient_2.getchoiceDoctor() == doctor_2.getMedical_specialties())) {
-		cout << patient_2.name << " " << patient_2.lastname << " - " << "Çäîðîâ"<<endl;
+		cout << patient_2.name << " " << patient_2.lastname << " - " << "Здоров"<<endl;
 		doctor_2.countOfCurePatient++;
 	}
 	else {
-		cout << "Íå òîò âðà÷ èëè íå òà ïîëèêëèíèêà";
+		cout << "Не тот врач или не та поликлиника";
 	}
 	return 0;
 }
